9652c58bcb28d30ed58d6c5ab36c965f66d6a02f
Optoed <origin-optoed@mail.ru>
2024-11-12 21:10:08 +0400

    0.4.0: added store.ts

---
+++ src/store.ts
@@ -0,0 +1,14 @@
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './features/auth/authSlice';
+import settingsReducer from './features/settings/settingsSlice';
+
+const store = configureStore({
+    reducer: {
+        auth: authReducer,
+        settings: settingsReducer,
+    },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export default store;
