fe8a7a612a657a3a3aef46551d675b95792eecbb
Optoed <origin-optoed@mail.ru>
2024-12-04 16:59:56 +0400

    0.14.0: dashboard is working

--- src/service/userService.ts
+++ src/service/userService.ts
@@ -2,11 +2,15 @@
 
 import axios from 'axios';
 import { UserType } from '../types/userType';
+import api from '../api';
 
 // Simulate an API request to fetch all users
 export const getAllUsers = async (): Promise<{ data: UserType[] }> => {
     try {
-        const response = await axios.get('/all_users'); // Replace with actual endpoint
+        const token = localStorage.getItem('authToken');
+        const response = await api.get('/all_users', {
+            headers: { Authorization: `Bearer ${token}` }
+        }); // Replace with actual endpoint
         return { data: response.data };  // Assuming the data returned is an array of users
     } catch (error) {
         throw new Error('Error fetching users');
